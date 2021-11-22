000b84eea41c9a3a99ef5d9ad04b69f486237fcd
mawriatty <gurau.cristi14@gmail.com>
2021-11-22 18:25:46 +0200

    first commit

---
+++ pages/theme.ts
@@ -0,0 +1,18 @@
+// Create a theme instance.
+import { createTheme } from '@mui/material/styles';
+
+const palette = {
+  primary: {
+    main: '#3A5AF9',
+  }
+};
+
+const theme = createTheme({
+  palette: { ...palette },
+  typography: {
+    fontFamily: '"Epilogue", "Helvetica", "Arial", sans-serif',
+    fontSize: 14,
+  },
+});
+
+export default theme;
